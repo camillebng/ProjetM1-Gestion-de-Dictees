644be66bcc1a459f36b55341f2eea348244276df
camillebng <cbenoitguyod@gmail.com>
2026-04-12 12:17:01 +0200

    Correction d'erreurs dans la requête sql

--- projet-dictees/php/liste_dict.php
+++ projet-dictees/php/liste_dict.php
@@ -1,18 +1,22 @@
 <?php
 $host = 'localhost';
 $dbname = 'gr4m1IDL';
-$username = 'm2dilipem':
+$username = 'm2dilipem';
 $password = 'm2dilipem';
 
 
 try{
-    $pdo = new PDO("mysql:host = $host;dbname = $dbname;charset = utf8",$username, $password);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8",$username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
-    die("Erreur de connexion : " / $e->getMessage());
+    die("Erreur de connexion : " . $e->getMessage());
 }
 
-$sql_query = $pdo -> query("SELECT p.titre, niveau, type, e.date FROM version_eleve e, version_prof p ORDER BY e.date DESC");
+$sql_query = $pdo -> query("SELECT p.titre, p.niveau, p.type, e.date 
+                            FROM version_prof p 
+                            INNER JOIN version_eleve e ON p.id_dict = e.dict_fk 
+                            ORDER BY e.date DESC");
+                            
 $dictees = $sql_query->fetchAll(PDO::FETCH_ASSOC);
 
 
